bcdb72f77b334504a55907ba97fbb73972b0f9da
samvelMelikyan <samvel.melikyan.eng@gmail.com>
2025-05-30 22:27:16 +0400

    Enhance Playwright tests to include login functionality for authenticated scenarios

--- tests/recently_added.spec.ts
+++ tests/recently_added.spec.ts
@@ -1,10 +1,14 @@
 import { test, expect} from '@playwright/test';
 
 test.describe('Recently Added', () => {
-  test.beforeEach(async ({ page }) => {
-    // await page.pause();
+  test.beforeEach(async ({ page }, testInfo) => {
     await page.goto('https://kinodaran.com/');
-    await page.getByRole('button', { name: 'Start Watching' }).click();
+
+    if (testInfo.title.includes('loggedIn')) {
+      logIn(page, 'msla@mail.ru', 'qazwsx123!@#');
+    } else { 
+      await page.getByRole('button', { name: 'Start Watching' }).click();
+    }
     await page.waitForTimeout(10000); // Wait for the page to load
   });
   
@@ -26,9 +30,17 @@ test.describe('Recently Added', () => {
   });
 
 
+  
 
 
 });
+
+async function logIn(page, email: string, password: string){
+  await page.getByRole('button', { name: 'Sign In' }).click();
+  await page.getByRole('dialog', { name: 'Example Modal' }).locator('#email').fill(email);
+  await page.getByRole('textbox', { name: 'Password *' }).fill(password);
+  await page.getByRole('button', { name: 'Sign In' }).click();
+}
 
 // await expect (page.locator('.action-btns > button').first()).toBeVisible();
 // await expect ( page.locator('.media-rating-button-group > button').first()).toBeVisible();
